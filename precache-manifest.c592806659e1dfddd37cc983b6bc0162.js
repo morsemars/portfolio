36ed91620ec315ee1f3f0ce5c8c1c572ccb6d37b
Marcelino Madriaga <madriaga.marcelino.jr@gmail.cpm>
2019-03-19 13:48:57 +0800

    Updates

---
+++ precache-manifest.c592806659e1dfddd37cc983b6bc0162.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "7707678f2405b7200447",
+    "url": "/github.io/static/js/runtime~main.50f22872.js"
+  },
+  {
+    "revision": "3771c001d88fa57fd761",
+    "url": "/github.io/static/js/main.c75530f0.chunk.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/github.io/static/js/2.f213e47f.chunk.js"
+  },
+  {
+    "revision": "04e4c6b2bd5c70a535283de429c1881a",
+    "url": "/github.io/index.html"
+  }
+];
