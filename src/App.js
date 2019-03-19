f8405d2a3514aa0a6ccdcb6d3b63c57a026e7145
Marcelino Madriaga <madriaga.marcelino.jr@gmail.cpm>
2019-03-19 15:59:32 +0800

    mvp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
 import React, { Component } from "react";
 
+import "./App.css";
+
 class App extends Component {
   render() {
-    return <div className="App">Under Construction...</div>;
+    return (
+      <div className="App">
+        <div className="name-container">
+          <h1>Marcelino Madriaga Jr.</h1>
+        </div>
+
+        <div className="content-container">
+          <h3>Software Engineer</h3>
+
+          <div className="icon-links">
+            <a href="https://github.com/morsemars">
+              <i class="fab fa-github" />
+            </a>
+          </div>
+        </div>
+
+        <div className="content-container">
+          <h3>Musician</h3>
+          <div className="icon-links">
+            <a href="https://www.instagram.com/morsemars">
+              <i class="fab fa-instagram" />
+            </a>
+
+            <a href="https://www.youtube.com/user/cadt08">
+              <i class="fab fa-youtube" />
+            </a>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
